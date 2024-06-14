74f547d1424c3b9de09b971019f5e33d42866573
Victor Glomba <victorglomba@gmail.com>
2024-06-14 17:20:45 -0300

    saco colors de coneccionDB

--- database/config.js
+++ database/config.js
@@ -1,14 +1,14 @@
 const mongoose= require("mongoose");
 
-require("colors")
+
 
 const dbConnection= async()=> {
     try {
         await mongoose.connect( process.env.MONGODB_CNN);
-            console.log("Base de datos".yellow, "--online--".green)
+            console.log("Base de datos", "--online--")
     } catch (error) {
         console.log(error);
-        throw new Error("Error a la hora de iniciar la base de datos".red);
+        throw new Error("Error a la hora de iniciar la base de datos");
     }
 }
 
